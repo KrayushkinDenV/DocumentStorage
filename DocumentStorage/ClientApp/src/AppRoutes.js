ce7c4a715c1716a899ce7439b34700dbf3b3a9de
krayushkin <KrayushkinDenV@yandex.ru>
2023-02-18 13:39:04 +0300

    Create DocumentStorage.js page based react-component.

--- DocumentStorage/ClientApp/src/AppRoutes.js
+++ DocumentStorage/ClientApp/src/AppRoutes.js
@@ -1,20 +1,25 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { DocumentStorage } from "./components/DocumentStorage";
 
 const AppRoutes = [
   {
-    index: true,
-    element: <Home />
+        index: true,
+        element: <Home />
   },
   {
-    path: '/counter',
-    element: <Counter />
+        path: '/counter',
+        element: <Counter />
   },
   {
-    path: '/fetch-data',
-    element: <FetchData />
-  }
+        path: '/fetch-data',
+        element: <FetchData />
+   },
+   {
+        path: '/documentStorage',
+        element
+    }
 ];
 
 export default AppRoutes;
---
+++ DocumentStorage/ClientApp/src/components/DocumentStorage.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+export class DocumentStorage extends Component {
+    static displayName = DocumentStorage.name;
+
+    render() {
+        return (
+            < div >
+                Новая страница 
+            </div>
+        );
+    }
+}
