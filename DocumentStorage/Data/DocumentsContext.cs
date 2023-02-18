2c14f70e04e75e6c8548e2a71732df3c7e708aea
krayushkin <KrayushkinDenV@yandex.ru>
2023-02-18 13:23:36 +0300

    Create document model

---
+++ DocumentStorage/Data/DocumentsContext.cs
@@ -0,0 +1,35 @@
+﻿//---Models---//
+using DocumentStorage.Models;
+//---NuGet---//
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DocumentStorage.Data
+{
+	public class DocumentsContext : DbContext
+	{
+		public DocumentsContext(DbContextOptions<DocumentsContext> options) : base(options)
+		{
+
+		}
+		public DbSet<Author> Authors { get; set; }
+		public DbSet<Achievement> Achievements { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			//var keysProperties = builder.Model.GetEntityTypes()                      // All entities
+			//					 .Select(x => x.FindPrimaryKey())        // All primary keys in entities
+			//					 .Where(key => key is { })               // Where key is ''
+			//					 .SelectMany(x => x.Properties);
+
+			//foreach (var property in keysProperties)
+			//{
+			//	property.ValueGenerated = ValueGenerated.OnAdd;
+			//}
+			modelBuilder.Entity<Author>().ToTable("Author");
+			modelBuilder.Entity<Achievement>().ToTable("Achievement");
+		}
+	}
+}
---
+++ DocumentStorage/Migrations/20230125210255_InitialModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentStorage.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Achievement",
+                columns: table => new
+                {
+                    AchievementId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FullTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JournalName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LinkToSource = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    AchievementType = table.Column<int>(type: "int", nullable: false),
+                    Documents = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Achievement", x => x.AchievementId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    AuthorId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Patronymic = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.AuthorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AchievementAuthor",
+                columns: table => new
+                {
+                    AchievementsAchievementId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AuthorsAuthorId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AchievementAuthor", x => new { x.AchievementsAchievementId, x.AuthorsAuthorId });
+                    table.ForeignKey(
+                        name: "FK_AchievementAuthor_Achievement_AchievementsAchievementId",
+                        column: x => x.AchievementsAchievementId,
+                        principalTable: "Achievement",
+                        principalColumn: "AchievementId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AchievementAuthor_Author_AuthorsAuthorId",
+                        column: x => x.AuthorsAuthorId,
+                        principalTable: "Author",
+                        principalColumn: "AuthorId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AchievementAuthor_AuthorsAuthorId",
+                table: "AchievementAuthor",
+                column: "AuthorsAuthorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AchievementAuthor");
+
+            migrationBuilder.DropTable(
+                name: "Achievement");
+
+            migrationBuilder.DropTable(
+                name: "Author");
+        }
+    }
+}
